e2fb3d8fb7cb75c0ea545f7e5de48944ce04403a
A S U S <borodichyulia18@gmail.com>
2022-06-06 14:17:06 +0300

    add entitu to resume

---
+++ src/entity/Resume.ts
@@ -0,0 +1,51 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Resume {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    secondName: string
+    
+    @Column({"type": Date})
+    dateofBirth: string
+
+    @Column()
+    gender: string
+
+    @Column()
+    email: string
+    
+    @Column()
+    country: string
+    
+    @Column()
+    placeOfEducation: string
+    
+    @Column()
+    periodOfEducation: string
+
+    @Column()
+    specialization: string
+
+    @Column()
+    prevCompany: string
+
+    @Column()
+    periodOfWork: string
+
+    @Column()
+    profession: string
+
+    @Column()
+    generalInfo: string
+    
+    @Column()
+    contacts: string 
+
+}
