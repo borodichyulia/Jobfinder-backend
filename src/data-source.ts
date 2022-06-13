815d00a2bc58fdc0b3a85ec9a4d4a7ad34d21ba2
A S U S <borodichyulia18@gmail.com>
2022-06-13 14:00:08 +0300

    company and applicant entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,9 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import { Resume } from "./entity/Resume"
+import { Vacancy } from "./entity/Vacancy"
+import { CompanyProfile } from "./entity/CompanyProfile"
+import { ApplicantProfile } from "./entity/ApplicantProfile"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -12,7 +15,7 @@ export const AppDataSource = new DataSource({
     database: "postgres",
     synchronize: true,
     logging: false,
-    entities: [Resume],
+    entities: [Resume, Vacancy, CompanyProfile, ApplicantProfile],
     migrations: [],
     subscribers: [],
 })
